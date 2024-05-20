1dc41a08360411fe4b5cee54ff2e298c8309c4e6
ashwekkalgutkar <72547233+ashwekkalgutkar@users.noreply.github.com>
2024-05-20 20:25:02 +0530

    seo updated

--- src/utils/portfolio.ts
+++ src/utils/portfolio.ts
@@ -37,7 +37,7 @@ export const seoData = {
     'Ashwek Kalgutkar is a full-stack developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
   author: author.name,
   image:
-    'https://user-images.githubusercontent.com/85820227/231264058-df236c47-a8d2-4077-9fa6-3577aa18e8ca.png',
+    'https://avatars.githubusercontent.com/u/72547233?v=4',
   url: '',
   keywords: [
     'Ashwek',
